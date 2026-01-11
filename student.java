ff8d4ad04999aa8ce82f6a67a40e86d15f9ab441
saquib27 <saquibshamshi27@gmail.com>
2026-01-11 12:35:44 +0530

    student

---
+++ student.java
@@ -0,0 +1,63 @@
+class StudentInfo {
+    private String name;
+    private int rollnumber;
+    private int grade;
+
+    StudentInfo(String name, int rollnumber, int grade) {
+        this.name = name;
+        this.rollnumber = rollnumber;
+        this.grade = grade;
+    }
+    StudentInfo(String name, int rollnumber) {
+        this.name = name;
+        this.rollnumber = rollnumber;
+        this.grade=22;
+    }
+    void displaydetails() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Roll Number: " + this.rollnumber);
+        System.out.println("Grade: " + this.grade);
+    }
+
+
+    void isPassed() {
+        if(this.grade>=40){
+            System.out.println("passed");
+        }
+        else{
+            System.out.println("failed");
+        }
+    }
+
+    void gracemark(){
+        this.grade=grade+10;
+
+    }
+
+}
+
+class student {
+
+    public static void main(String[] args) {
+        StudentInfo s1 = new StudentInfo("saquib", 101, 75);
+        StudentInfo s2= new StudentInfo("sushant", 102, 33);
+        StudentInfo s3 = new StudentInfo("peter", 22);
+
+        s1.gracemark();
+        s1.displaydetails();
+        s1.isPassed();
+        
+
+        System.out.println("\n");
+         s2.gracemark();
+         s2.displaydetails();
+         s2.isPassed();
+
+          System.out.println("\n");
+          s3.displaydetails();
+          s3.isPassed();
+
+
+        
+    }
+}
